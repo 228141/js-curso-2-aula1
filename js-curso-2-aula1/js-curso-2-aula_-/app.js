553688060a4a7112cdf0cada944bbea567cde011
228141 <jailton.sp@hotmail.com>
2024-11-13 08:59:44 -0300

    Funções

--- js-curso-2-aula1/js-curso-2-aula_-/app.js
+++ js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -4,14 +4,31 @@
 //let paragrafo = document.querySelector('p');
 //paragrafo.innerHTML = 'Escolha um Número entre 1 e 10';
 
+let numeroSecreto = gerarNumeroAleatorio();
+
 function exibirTextoNaTela(tag, texto) {
-    let campo = document.querySelector(tag)
-    campo.innerHTML = texto     
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;     
 }
 
 exibirTextoNaTela('h1', 'Jogo do Número Secreto');
 exibirTextoNaTela('p', 'Escolha um Número entre 1 e 100');
 
 function verificarChute() {
-    console.log("O botão foi clicado")
+    let chute = document.querySelector('input').value;
+
+    if (chute == numeroSecreto){
+        exibirTextoNaTela('h1','Acertou');
+        exibirTextoNaTela('p', 'Você descobriu o número secreto');
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'O número Secreto Menor');
+        } else {
+            exibirTextoNaTela('p', 'O número Secreto é Maior');
+        }
+    }
+}
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
 }
